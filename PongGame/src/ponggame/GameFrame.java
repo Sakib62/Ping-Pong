7e2a17353d67f4f2e1b2efcae76b4b03e2544adb
Sakibul Islam <67078056+Sakib62@users.noreply.github.com>
2022-08-09 23:52:28 +0600

    Created Initial GameFrame

--- PongGame/src/ponggame/GameFrame.java
+++ PongGame/src/ponggame/GameFrame.java
@@ -7,5 +7,14 @@ public class GameFrame extends JFrame {
     
     GameFrame() {
         panel = new GamePanel();
+        
+        this.setTitle("Pong Game");
+        this.setResizable(false);
+        this.setBackground(Color.black);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+
     }
 }
