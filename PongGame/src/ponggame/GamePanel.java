ee7d9f35d157914ab6f23073ce37ef5c6a11cae1
Safayet Rafi <safayetrafi461@gmail.com>
2022-08-12 21:56:19 +0600

    Completed GamPanel Constructor

--- PongGame/src/ponggame/GamePanel.java
+++ PongGame/src/ponggame/GamePanel.java
@@ -26,8 +26,17 @@ public class GamePanel extends JPanel implements Runnable {
     Score score;
 
     
-    GamePanel() {
-
+    GamePanel() 
+    {
+        newBall();
+        newPaddles();
+        score = new Score();
+        this.setFocusable(true);
+        this.addKeyListener(new AL());
+        this.setPreferredSize(SCREEN_SIZE);
+        
+        gameThread = new Thread(this);
+        gameThread.start();
     }
 
     public void newBall() {
