f75fc90b9973cecbe073c071be71128adbf41fd9
Sakibul Islam <67078056+Sakib62@users.noreply.github.com>
2022-08-13 17:48:53 +0600

    Added gameloop in GamePanel class

--- PongGame/src/ponggame/GamePanel.java
+++ PongGame/src/ponggame/GamePanel.java
@@ -67,7 +67,23 @@ public class GamePanel extends JPanel implements Runnable {
     }
 
     public void run() {
-
+        
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000/amountOfTicks;
+        double delta = 0;
+        
+        while(true) {
+            long now = System.nanoTime();
+            delta += (now - lastTime)/ns;
+            lastTime = now;
+            if(delta>=1) {
+                move();
+                checkCollision();
+                repaint();
+                delta--;
+            }
+        }
     }
 
     public class AL extends KeyAdapter {
